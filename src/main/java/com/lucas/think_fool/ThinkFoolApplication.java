2cc8214cb3121635485c683bcf965c8e0e43fbf7
Lucas Maciel <lucasmacielcontato@gmail.com>
2025-01-25 22:33:08 -0300

    feat: env vars

--- src/main/java/com/lucas/think_fool/ThinkFoolApplication.java
+++ src/main/java/com/lucas/think_fool/ThinkFoolApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
  
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@PropertySource("file:${user.dir}/.env")
 public class ThinkFoolApplication {
 
 	public static void main(String[] args) {
